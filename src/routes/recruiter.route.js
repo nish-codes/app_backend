a9c801219f6ae9fb2a9d5284f1422ada71863245
Nishanth <nishanth24106@gmail.com>
2025-09-14 21:06:42 +0530

    additional controller for recruiter and student

--- src/routes/recruiter.route.js
+++ src/routes/recruiter.route.js
@@ -1,7 +1,29 @@
 import { Router } from "express";
-import { recruiterLogin, recruiterSignup } from "../controllers/recruiter.controller.js";
+import { verifyFirebaseToken } from "../middlewares/verifyFirebaseToken.js";
+import { 
+  recruiterLogin, 
+  recruiterSignup, 
+  postJob, 
+  getApplications, 
+  updateRecruiterProfile, 
+  getRecruiterAnalytics 
+} from "../controllers/recruiter.controller.js";
 
 const route = Router()
+
+// Authentication routes
 route.route("/signup").post(verifyFirebaseToken, recruiterSignup);
 route.route("/login").post(verifyFirebaseToken, recruiterLogin);
+
+// Job management routes
+route.route("/jobs").post(verifyFirebaseToken, postJob);
+route.route("/jobs/:jobId/applications").get(verifyFirebaseToken, getApplications);
+
+// Profile management
+route.route("/profile").put(verifyFirebaseToken, updateRecruiterProfile);
+
+// Analytics
+route.route("/analytics").get(verifyFirebaseToken, getRecruiterAnalytics);
+
+export default route;
 
