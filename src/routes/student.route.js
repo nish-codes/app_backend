084c71661742d4a2bd78cd9bb9042f987e1758a8
Harish Siddartha <harishranjithkumar@gmail.com>
2025-10-02 23:14:23 +0530

    error in routes fixed

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyFirebaseToken } from "../middlewares/verifyFirebaseToken.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-import { signup,checkUser, login, getJobs, getHackathons, applyToJob, updateStudentProfile , uploadProfilePhoto, getStudentDetails, verifySkill, addSkill } from "../controllers/student.controller.js";
+import { signup, checkUser, login, getJobs, getHackathons, applyToJob, updateStudentProfile, uploadProfilePhoto, getStudentDetails, verifySkill, addSkill } from "../controllers/student.controller.js";
 
 const router = Router();
 
@@ -21,10 +21,10 @@ router.post(
 );
 
 //skills route
-router.post('/verifySkills',verifyFirebaseToken,verifySkill)
-router.post('/addSkills',verifyFirebaseToken,addSkill)
+router.post('/verifySkills', verifyFirebaseToken, verifySkill)
+router.post('/addSkills', verifyFirebaseToken, addSkill)
 // ✅ Data fetching routes  
-router .post('./StudentDetails',verifyFirebaseToken,getStudentDetails)
+router.post('/StudentDetails', verifyFirebaseToken, getStudentDetails)
 // Data fetching routes  
 router.get("/jobs", verifyFirebaseToken, getJobs);
 router.get("/hackathons", verifyFirebaseToken, getHackathons);
