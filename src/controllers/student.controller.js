a82db49731903b221485520202446f0b603bc159
Roshan Mayengbam <roshanmayengbm@gmail.com>
2025-10-02 11:25:59 +0530

    Save local changes before pull

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -957,15 +957,15 @@ export {
   getStudentDetails,
   addSkill,
   verifySkill,
-<<<<<<< HEAD
+
   fetchsaves,
-  fetchAppliedJobs
-=======
+  fetchAppliedJobs,
+
   getApplicationCounts,
   getAppliedApplicationsCount,
   getShortlistedApplicationsCount,
   getApplications,
   getStudentAnalytics,
->>>>>>> 7ea7f53039015b64c5708fdfd1ddd53ee2a12128
-  // getStudentAnalytic
+
+
 };
